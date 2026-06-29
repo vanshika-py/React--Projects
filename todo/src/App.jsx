ebf74670c5b6fb635a96645e4038e5de4db8d3b7
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-06-29 22:49:21 +0530

    fixed css

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import React from 'react'
-
+import Todo from './components/Todo'
+import "./App.css";
 
 const App = () => {
   return (
+<div>
 
-
-
-  
+  <Todo/>
 
 
   </div>
---
+++ todo/src/components/Todo.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const Todo = () => {
+  return (
+    
+         <div className="min-h-screen bg-blue-800 p-5 from-slate-900 via-slate-800 to-black flex flex-col items-center pt-16">
+      
+      <header className="text-5xl text-white font-bold">
+        TO DO LIST
+      </header>
+
+      <form className="flex w-full max-w-md shadow-lg rounded-xl overflow-hidden m-5">
+        <input
+          type="text"
+          placeholder="Enter your task..."
+          className="flex-1 px-4 py-3 text-lg outline-none bg-white"
+        />
+
+        <button
+          type="submit"
+          className="px-6 bg-red-400 text-white font-semibold hover:bg-green-400 transition duration-300"
+        >
+          Add TASK
+        </button>
+      </form>
+    </div>
+    )
+    }
+  
+
+export default Todo
