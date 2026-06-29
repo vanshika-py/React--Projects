8a0892c722745e421d12e30b3ae304ccd0894c27
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-06-29 23:47:45 +0530

    added form events to prevent its default bheaviour

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { useState } from 'react';
 
 const Todo = () => {
 const [inputValue, setinputValue] = useState("");
 
+function handleInput(event){
+  setinputValue(event.target.value)
+}
+
+function handleFormSubmit (event){
+     event.preventDefault()
+}
 
 
   return (
@@ -11,14 +18,16 @@ const [inputValue, setinputValue] = useState("");
          <div className="min-h-screen bg-blue-800 p-5 from-slate-900 via-slate-800 to-black flex flex-col items-center pt-16">
       
       <header className="text-5xl text-white font-bold">
-        TO DO LIST
+        TO DO LIST✔️
       </header>
 
-      <form className="flex w-full max-w-md shadow-lg rounded-xl overflow-hidden m-5">
+      <form onSubmit={handleFormSubmit} className="flex w-full max-w-md shadow-lg rounded-xl overflow-hidden m-5">
         <input
           type="text"
           placeholder="Enter your task..."
           className="flex-1 px-4 py-3 text-lg outline-none bg-white"
+          value = {inputValue}
+          onChange={handleInput}
         />
 
         <button
